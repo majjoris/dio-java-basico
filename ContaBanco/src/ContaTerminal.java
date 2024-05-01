37fb2a6315fcce518b3619d14b6555ac56e8043a
Euller Miranda <49294802+majjoris@users.noreply.github.com>
2024-05-01 11:08:53 -0300

    final

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner ler = new Scanner(System.in);
+        
+        int numero;
+        String agencia;
+        String nome_cliente;
+        float saldo;
+        
+        System.out.println("Por favor, digite o nome do cliente !");
+        nome_cliente=ler.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        numero=ler.nextInt();
+
+        System.out.println("Por favor, digite qual é a Agência !");
+        agencia=ler.next();
+
+        System.out.println("Por favor, digite o saldo da conta !");
+        saldo=ler.nextFloat();
+
+        ler.close();
+
+        System.out.println("Olá "+ nome_cliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
     }
 }
